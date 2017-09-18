4d4711b71c5a11c2390ad0ab53d6098677d4084f
vagrant <thecarloferrer@gmail.com>
2017-09-18 23:08:47 +0000

    Change colour of char-counter when below zero. Change back when zero or above.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,5 +2,10 @@ $(document).ready(function() {
   $('.new-tweet textarea').on('keyup', function() {
     let diff = $(this).val().length;
     $(this).parent().find('.counter').text(140 - diff);
+    if (diff > 140) {
+      $(this).parent().find('.counter').css({'color' : 'red'});
+    } else {
+      $(this).parent().find('.counter').css({'color' : '#244751'});
+    }
   });
 });
