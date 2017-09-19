e48e4b393bf00be3ab96ec64af3015dc1ddc7709
vagrant <thecarloferrer@gmail.com>
2017-09-19 21:56:38 +0000

    Now we're adding tweets via AJAX!

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,56 +7,21 @@
 
 
 $(document).ready(function() {
-  // Fake data taken from tweets.json
-  var data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": {
-          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-        },
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": {
-          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-        },
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-    {
-      "user": {
-        "name": "Johann von Goethe",
-        "avatars": {
-          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-        "handle": "@johann49"
-      },
-      "content": {
-        "text": "Es ist nichts schrecklicher als  tätige Unwissenheit."
-      },
-      "created_at": 1461113796368
-    }
-  ];
 
-  renderTweets(data);
   formSubmission();
+  loadTweets();
+
+  function loadTweets() {
+    $.ajax({
+      url: 'http://localhost:8080/tweets',
+      method: 'GET',
+      success: function (firstTweets) {
+        // $('#tweet-stack').replaceWith(firstTweets);
+        renderTweets(firstTweets);
+        // console.log(firstTweets);
+      }
+    });
+  }
 
   function formSubmission() {
     $('.container')
@@ -65,7 +30,9 @@ $(document).ready(function() {
     .on('submit', function(event) {
       event.preventDefault();
       console.log($(this).serialize());
+      // loadTweets($(this).serialize());
     });
+    return;
   }
 
   function renderTweets(data) {
