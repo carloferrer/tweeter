bbcbaf1b7e72e36ceaaafb4c17d181b08c293d30
vagrant <thecarloferrer@gmail.com>
2017-09-19 17:00:56 +0000

    Made all css styling for single tweet positioned relative to tweet-box.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,6 +4,32 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-function createTweetElement(obj) {
-
+// Test / driver code (temporary). Eventually will get this from the server.
+var tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": {
+      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+    },
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
 }
+
+$(document).ready(function() {
+  function createTweetElement() {
+    let $newTweet = $('<article>').addClass('tweet-box');
+    $newTweet.text('TEST');
+    return $newTweet;
+  }
+  $('#tweet-stack').append(createTweetElement);
+});
+
+// Test / driver code (temporary)
+// console.log(tweet); // to see what it looks like
+// $('#tweet-stack').append(tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready( function() {
   $('.new-tweet textarea').on('keyup', function() {
     let diff = $(this).val().length;
     $(this).parent().find('.counter').text(140 - diff);
